438cac9260059897ecdfe60c1f7d0f318a54b0dc
Abid binjahangir <abinjahangir41@gmail.com>
2026-06-25 04:07:00 +0600

    Add files via upload

---
+++ Lab-1/task1.java
@@ -0,0 +1,23 @@
+public class task1{
+    public static void main(String[] args) {
+       
+        String name = "Abid";
+        String studentId = "E253002";
+        int age = 21;
+        String email = "abid.com";
+        String phone = "01568495336";
+        String address = "ctg";
+
+       
+        System.out.println("========================================");
+        System.out.println("          STUDENT INFORMATION           ");
+        System.out.println("========================================");
+        System.out.println("Name:       " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Age:        " + age);
+        System.out.println("Email:      " + email);
+        System.out.println("Phone:      " + phone);
+        System.out.println("Address:    " + address);
+        System.out.println("========================================");
+    }
+}
